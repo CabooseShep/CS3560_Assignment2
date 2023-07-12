6afc1853484ef1f86cc0df9af178338cf0849798
Nick Hortua <nhortua@cpp.edu>
2023-07-12 05:54:15 -0700

    part b complete

---
+++ Question5/part_b/src/Book.java
@@ -0,0 +1,12 @@
+/* Author: Nick Hortua
+ *  Project: CS 3560 Assignment 2 Question 5a
+ *  Implementation of Book class for the required task from Question 5b.  */
+public class Book {
+    private String name;
+    private String author;
+    private Course course;
+
+    public Book(){
+        System.out.println("Constructor created \n");
+    }
+}
---
+++ Question5/part_b/src/Course.java
@@ -0,0 +1,12 @@
+/* Author: Nick Hortua
+ *  Project: CS 3560 Assignment 2 Question 5a
+ *  Implementation of Course class for the required task from Question 5b.  */
+public class Course {
+    private int code;
+    private Book book;
+
+    public Course(int code, Book book){
+        this.code = code;
+        this.book = book;
+    }
+}

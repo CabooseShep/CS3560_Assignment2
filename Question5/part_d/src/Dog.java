569fa536f29fdba904952ff51e0d287bd3ba4bcb
Nick Hortua <nhortua@cpp.edu>
2023-07-12 06:27:12 -0700

    part d complete

---
+++ Question5/part_d/src/Dog.java
@@ -0,0 +1,9 @@
+import java.util.List;
+public class Dog {
+    private String breed, name;
+    private List<Paw> listPaws;
+
+    public Dog(){
+        System.out.println("Constructor created");
+    }
+}

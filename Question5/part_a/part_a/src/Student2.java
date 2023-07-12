11d966353fc836b54324496cecd69389334da1b6
Nick Hortua <nhortua@cpp.edu>
2023-07-12 05:44:29 -0700

    part a complete

---
+++ Question5/part_a/part_a/src/Student2.java
@@ -0,0 +1,13 @@
+/* Author: Nick Hortua
+ *  Project: CS 3560 Assignment 2 Question 5a
+ *  Implementation of Student2 class for the required task from Question 5a.  */
+public class Student2 {
+    private String name;
+    private Transcript transcript;
+
+
+    public Student2(String name, Transcript transcript){
+        this.name = name;
+        this.transcript = transcript;
+    }
+}
---
+++ Question5/part_a/part_a/src/Transcript.java
@@ -0,0 +1,12 @@
+/* Author: Nick Hortua
+ *  Project: CS 3560 Assignment 2 Question 5a
+ *  Implementation of Transcript class for the required task from Question 5a.  */
+public class Transcript {
+    private String course;
+    private double grade;
+
+    public Transcript(String course, double grade){
+        this.course = course;
+        this.grade = grade;
+    }
+}

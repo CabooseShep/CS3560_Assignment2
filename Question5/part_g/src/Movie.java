bfc897e3ca80c76cebaae56a3bd440ef6e45ceb2
Nick Hortua <nhortua@cpp.edu>
2023-07-12 07:08:11 -0700

    part g complete

---
+++ Question5/part_g/src/Movie.java
@@ -0,0 +1,10 @@
+import java.util.List;
+public class Movie {
+    private String name, genre;
+    private List<watch> listWatch;
+
+    public Movie(String name, String genre){
+        this.name = name;
+        this.genre = genre;
+    }
+}
---
+++ Question5/part_g/src/Person.java
@@ -0,0 +1,9 @@
+import java.util.List;
+public class Person {
+    private String name;
+    private List<watch> listWatch;
+
+    public Person(String name){
+        this.name = name;
+    }
+}
